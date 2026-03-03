581c83fb7623718875b69198f3948cd2c2dba422
Vinodh <gowdavinod995@gmail.com>
2026-03-03 23:00:36 +0530

    we are started Payment-Validation-Service

---
+++ Payment-Validation-Service/src/test/java/com/hulkhiretech/PaymentValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulkhiretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
